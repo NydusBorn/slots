159936e2913ffbc0c65fab628b6e9e102aa675ba
Nydus Born <90088743+NydusBorn@users.noreply.github.com>
2024-01-15 22:08:02 +0300

    Root layout

---
+++ src/app/game/page.tsx
@@ -0,0 +1,14 @@
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'Slots: Game',
+    description: 'Slots: Game',
+}
+
+export default function Home() {
+    return (
+        <>
+            <p>Game</p>
+        </>
+    )
+}
---
+++ src/app/settings/page.tsx
@@ -0,0 +1,14 @@
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'Slots: Settings',
+    description: 'Slots: Settings',
+}
+
+export default function Home() {
+    return (
+        <>
+            <p>Settings</p>
+        </>
+    )
+}
